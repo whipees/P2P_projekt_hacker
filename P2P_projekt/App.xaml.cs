3f2050045d7487c68ffd7f91d1abd8fdc00ef7e3
sebwhi <sebikjanecek@gmail.com>
2026-01-26 22:23:42 +0100

    Přidejte soubory projektu.

---
+++ P2P_projekt/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace P2P_projekt
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
