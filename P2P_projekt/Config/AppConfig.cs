12c5b606688d4c3c9cbf739b582b77651dda362e
sebwhi <sebikjanecek@gmail.com>
2026-01-26 22:39:37 +0100

    added config

--- P2P_projekt/Config/AppConfig.cs
+++ P2P_projekt/Config/AppConfig.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace P2P_projekt.Config
 {
-    class AppConfig
+    public static class AppConfig
     {
+        public static string IpAddress { get; } = GetLocalIp();
+        public static int Port { get; set; } = 65530;
+        public static int Timeout { get; set; } = 5000;
+
+        private static string GetLocalIp()
+        {
+            try
+            {
+                using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0);
+                socket.Connect("8.8.8.8", 65530);
+                if (socket.LocalEndPoint is IPEndPoint endPoint)
+                {
+                    return endPoint.Address.ToString();
+                }
+                return "127.0.0.1";
+            }
+            catch
+            {
+                return "127.0.0.1";
+            }
+        }
     }
 }
