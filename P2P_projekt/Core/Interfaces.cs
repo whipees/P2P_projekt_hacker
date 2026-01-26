1f2787b13f03fa9c46d20dc95e9de4289b8fe654
sebwhi <sebikjanecek@gmail.com>
2026-01-26 22:32:29 +0100

    initial commit

---
+++ P2P_projekt/Core/Interfaces.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2P_projekt.Core
+{
+    public interface ICommand
+    {
+        string Execute();
+    }
+
+    public interface IBankObserver
+    {
+        void Update(long totalFunds, int totalClients);
+    }
+
+    public interface IStorage
+    {
+        void Save(Dictionary<int, long> accounts);
+        Dictionary<int, long> Load();
+    }
+}
