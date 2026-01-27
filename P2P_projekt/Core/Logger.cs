7a799aafaad4b49006a91a6a7db052b1fe38875e
DanOujesky <oujesky@spsejecna.cz>
2026-01-27 14:40:31 +0100

    implemented Logger singleton

--- P2P_projekt/Core/Logger.cs
+++ P2P_projekt/Core/Logger.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System.IO;
 
-namespace P2P_projekt.Core
+namespace P2PBankNode.Core
 {
-    internal class Logger
+    public sealed class Logger
     {
+        private static readonly Lazy<Logger> _lazy = new(() => new Logger());
+        public static Logger Instance => _lazy.Value;
+        private readonly object _lock = new();
+        private const string LogFile = "bank_node.log";
+
+        public void Log(string message)
+        {
+            lock (_lock)
+            {
+                try
+                {
+                    string entry = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {message}";
+                    File.AppendAllText(LogFile, entry + Environment.NewLine);
+                }
+                catch { }
+            }
+        }
+
+        public void Error(string message) => Log($"ERROR: {message}");
     }
 }
